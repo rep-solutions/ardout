727036de18142527b37b24a5f042a6643e83aec2
Rep-solutions <cox_marc@hotmail.com>
2025-09-18 23:56:33 +0100

    update namespace

--- R/hello.R
+++ R/hello.R
@@ -1,3 +1,11 @@
+#' Say hello
+#'
+#' @param name character; who to greet
+#' @return character
+#' @export
+#' @examples
+#' hello()
+#' hello("Marc")
 hello <- function(name = "world") {
   sprintf("Hello world, %s!", name)
 }
